b9a495ec116b7330e08996e89f79c588e6938b21
Yujev <ustatar@ya.ru>
2022-10-06 12:32:49 +0300

    Нашел сумму элементов на нечётных позициях

--- DZ_Zadacha_36/Program.cs
+++ DZ_Zadacha_36/Program.cs
@@ -6,11 +6,14 @@
 
 [-4, -6, 89, 6] -> 0
 */
-
+Console.WriteLine();
+Console.WriteLine("Задача 36: Найди сумму элементов, стоящих на нечётных позициях");
+Console.WriteLine();
 int[] array = GetArray(7, -15, 100);
-Console.WriteLine("Ваш массив: ");
+Console.WriteLine("Твой массив: ");
+Console.WriteLine();
 Console.WriteLine(String.Join(" ", array));
-
+Console.WriteLine();
 
 int[] GetArray(int size, int minValue, int maxValue)
 {
@@ -37,6 +40,6 @@ for (int i = 0; i < array.Length; i++)
 }
 
 Console.WriteLine($"Сумма элементов на  нечётных позициях: {oddNumbersCount}");
-
+Console.WriteLine();
 
 
